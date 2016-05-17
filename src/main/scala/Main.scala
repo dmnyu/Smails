a6965fab4e6a8421fd52a52e03b031dc855a0c24
Donald Mennerich <don.mennerich@gmail.com>
2016-05-17 09:17:44 -0400

    adding cli and password dialogue

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,59 +1,39 @@
-package ch.menneri.smails
+package ch.menneri.smail
 
 import com.typesafe.config.ConfigFactory
-import java.io.File
-import javax.mail._
 import javax.mail.internet.InternetAddress
+import org.rogach.scallop._
+import pl.project13.scala.rainbow._
 
-import net.fortuna.mstor._
+class CLIConf(arguments: Seq[String]) extends ScallopConf(arguments) {
+  val service = opt[String](required = true)
+  val email = opt[String](required = true)
+  verify()
+}
 
-object Main extends App {
+
+object Main extends App with SmailSupport {
+
+  println("smail v.0.1")
   val conf = ConfigFactory.load()
-
-  val service = args(0)
-  val account = args(1)
-  val password = args(2)
-
-  validateAddr(account) match {
-    case false => System.exit(1)
+  val cli = new CLIConf(args)
+  
+  validateAddr(cli.email()) match {
+    case false => println("email address is malformed"); System.exit(1)
     case _ =>
   }
 
-  val props = System.getProperties
-  val session = Session.getDefaultInstance(props, null)
-  val store = session.getStore("imaps")
-  store.connect(conf.getString(s"smail.$service"), conf.getInt("smail.port"), account, password)
-  
-  store.isConnected match {
-    case true => println("* connected to imap server")
-    case false => {
-      println("* could not connect to imap server")
-      System.exit(1)
-    }
-  }
+  val password = getPassword
 
-  val mailRoot = store.getDefaultFolder
-  val folders = mailRoot.list("*")
+  acquireAccount(conf.getString(s"smail.${cli.service()}"), conf.getInt("smail.port"), cli.email(), password)
 
-  folders.foreach{ folder =>
-    val imapFolder = folder.getName
-    folder.open(Folder.READ_ONLY)
-    val count = folder.getMessageCount
-    val messages = folder.getMessages
-    println(s"* writing $count messages to local mbox file")   
-    val mboxLocation = getMboxLocation("mbox", account, imapFolder)
-    val mbox = new Mbox(mboxLocation)
-    val mboxFolder = mbox.getMbox(imapFolder)
-    var i = 1
-    messages.foreach { message => 
-      println(s"* writing message $i/$count" + message.getSubject)
-      mboxFolder.appendMessages(Array(message))
-      i = i + 1
-    }
-    mbox.close
+  def getPassword(): String = {
+    println(s"enter the password for ${cli.email()}")
+    Console.out.print("" + Console.INVISIBLE)
+    val password = scala.io.StdIn.readLine()
+    Console.out.print("" + Console.RESET)
+    password
   }
-  
-  store.close
 
   def validateAddr(addr: String): Boolean = {
     val emailAddr = new InternetAddress(addr)
@@ -67,51 +47,7 @@ object Main extends App {
       }
     }
   }
-    
-  def getMboxLocation(root: String, addressee: String, folder: String): File = {
-    
-    val rootMbox = new File(root)
-    if(rootMbox.exists == false) rootMbox.createNewFile
-
-    val addr = addressee.split("@")(0) + "_AT_" + addressee.split("@")(1).split("\\.")(0) + "_DOT_" + addressee.split("@")(1).split("\\.")(1)
-    val accountFolder = new File(rootMbox, addr)
-    accountFolder.exists match {
-      case true => println(s"* folder for account $addr exists")
-      case false => {
-        accountFolder.mkdir
-        println(s"* account folder created for $addr")
-      }
-    }
-    val localMbox = new File(accountFolder, folder)
-
-    localMbox.exists match {
-      case true => println(s"* local mbox file $folder already exists")
-      case false => { 
-        localMbox.createNewFile
-        println(s"* local mbox file $folder created")
-      }
-    }
-
-    accountFolder
-  }
+  
 }
+    
 
-class Mbox(location: File) {
-
-  val mProps = System.getProperties
-  mProps.setProperty("mstor.mbox.metadataStrategy", "none")
-  val mSession = Session.getDefaultInstance(mProps)
-  val mStore = new MStorStore(mSession, new URLName("mstor:" + location.getAbsolutePath)) 
-  mStore.connect
-  val mboxRoot = mStore.getDefaultFolder
-
-  def close() {
-    mStore.close
-  }
-
-  def getMbox(name: String): Folder = {
-    val mFolder = mboxRoot.getFolder(name)
-    mFolder.open(Folder.READ_WRITE)
-    mFolder
-  }
-}
